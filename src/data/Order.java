c82ea681300d832cc5870a1e36357de36498995a
Muhammed Adel <muhammed.adel1997@gmail.com>
2020-07-22 00:25:22 +0200

    Updating Order.java.

--- src/data/Order.java
+++ src/data/Order.java
@@ -1,6 +1,21 @@
 package data;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class Order {
     private int orderId;
+    private ObservableList<OrderedItem> orderedItemList;
+    private int customerId;
+    private double grossTotal;
+    private double netTotal;
+    private double vat;
+    private String orderDate;
+
+    public Order(){
+        this.orderedItemList = FXCollections.observableArrayList();
+        this.grossTotal = 0;
+    }
+    
 
 }
