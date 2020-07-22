d9e6b1b3380d56ebca3e9c2944ab1ce2884c2a1b
Muhammed Adel <muhammed.adel1997@gmail.com>
2020-07-22 16:02:05 +0200

    TotalEntry.java complete

--- src/data/TotalEntry.java
+++ src/data/TotalEntry.java
@@ -1,4 +1,20 @@
 package data;
 
 public class TotalEntry {
+
+    double totalCost;
+    String desTotalCost;
+
+    public TotalEntry(double totalCost, String desTotalCost){
+        this.totalCost = totalCost;
+        this.desTotalCost = desTotalCost;
+    }
+
+    public String getDesTotalCost() {
+        return desTotalCost;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
 }
