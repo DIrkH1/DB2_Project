f82c3fbdc2ffa85abbb37764611ff0a1ff06f898
Muhammed Adel <muhammed.adel1997@gmail.com>
2020-07-22 15:59:31 +0200

    Updating Order.java. Neue Hilfsklasse TotalEntry.java

--- src/data/Order.java
+++ src/data/Order.java
@@ -16,6 +16,60 @@ public class Order {
         this.orderedItemList = FXCollections.observableArrayList();
         this.grossTotal = 0;
     }
-    
 
+    public Order( int orderId, int customerId, String orderDate){
+        this.orderedItemList = FXCollections.observableArrayList();
+        this.grossTotal = 0;
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.orderDate = orderDate;
+    }
+
+    public Order(int orderId, double netTotal, double grossTotal, int customerId , String orderDate ){
+        this.orderedItemList = FXCollections.observableArrayList();
+        this.orderId = orderId;
+        this.grossTotal = grossTotal;
+        this.netTotal = netTotal;
+        this.customerId = customerId;
+        this.orderDate = orderDate;
+    }
+
+    public void addProduct(Product product){
+        boolean itemExists = false;
+        for (OrderedItem ordItem : orderedItemList){
+            if(ordItem.equals(product)){
+                ordItem.incOrder();
+                itemExists = true;
+            }
+        }
+        if(!itemExists){
+            orderedItemList.add(new OrderedItem(product));
+        }
+        grossTotal = grossTotal + product.getPrice();
+        netTotal = this.getNetTotal();
+    }
+
+    public void removeOrderedItem(OrderedItem orderedItem){
+        if(orderedItemList.contains(orderedItem)){
+            if(orderedItem.getOrderAmount() > 1){
+                orderedItem.decOrder();
+            } else {
+                orderedItemList.remove(orderedItem);
+            }
+            grossTotal = grossTotal - orderedItem.getPrice();
+            netTotal = this.getNetTotal();
+        }
+    }
+
+    public ObservableList<OrderedItem> getOrderedItemList() {
+        return orderedItemList;
+    }
+
+    public ObservableList<TotalEntry> getTotalCost(){
+        ObservableList<TotalEntry> totalCostList = FXCollections.observableArrayList();
+        totalCostList.add(new TotalEntry(netTotal, "netTotal: "));
+        String vat = "grossTotal (16% tax): ";
+        totalCostList.add(new TotalEntry(grossTotal, vat));
+        return totalCostList;
+    }
 }
